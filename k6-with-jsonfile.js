ec4e22a5f56ad77e1fb25c9441dec6ba7c629674
KaushikTest <88329159+KaushikTest@users.noreply.github.com>
2023-05-27 15:06:58 +0530

    Refactor
    added script using json file

---
+++ k6-with-jsonfile.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+
+const jsondata=open('./test.json');
+const data=JSON.parse(jsondata);
+
+export default function () {
+    const index = __VU % data.number.length;
+    const item=data['number'][index];
+    console.log(`VU ${__VU} is processing item: ${item}`);
+    http.get('https://catfact.ninja/'+`${item}`);
+}
+
+/**
+ * Use command k6 run k6-with-sharedarray.js --vus 10 --iterations 10
+ */
