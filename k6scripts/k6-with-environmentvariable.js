ccecc904cf7e2c915db175fad0438153c2058a89
KaushikTest <88329159+KaushikTest@users.noreply.github.com>
2023-06-16 23:05:45 +0530

    added k6 script for running with environment variable

---
+++ k6scripts/k6-with-environmentvariable.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+    console.log(__ENV.windir)
+    http.get('https://catfact.ninja/fact');
+    sleep(1);
+}
+
+/**
+ * Use command k6 run k6-basic-script.js
+ */
